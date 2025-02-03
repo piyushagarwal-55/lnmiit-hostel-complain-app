ec99e43b72db56176bc331a20d06a680e1d3c1ea
piyushagarwal-55 <piyushaga2005@gmail.com>
2025-02-04 02:37:07 +0530

    vercel push10

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -12,7 +12,7 @@ router.get('/register', ensureNotAuthenticated, async (req, res, next) => {
 });
 
 router.post('/login', ensureNotAuthenticated, passport.authenticate('local', {
-  successRedirect: '/',
+  successRedirect: 'https://hostel-management-advanced.vercel.app/',
   failureRedirect: '/auth/login',  // Correctly redirects back to login page on failure
   failureFlash: true
 }));
@@ -61,7 +61,7 @@ router.post('/register', ensureNotAuthenticated, [
     const user = new User(req.body);
     await user.save();
     req.flash('success', 'Successfully registered! Please Log in...');
-    res.redirect('/auth/login');
+    res.redirect('https://hostel-management-advanced.vercel.app/auth/login');
   } catch (error) {
     next(error);
   }
