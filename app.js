00835b076e7bcfb87b156dacbc23f99f50d94ea2
piyushagarwal-55 <piyushaga2005@gmail.com>
2025-02-04 03:01:55 +0530

    vercel push13

--- app.js
+++ app.js
@@ -55,7 +55,11 @@ app.use((req, res, next) => {
   next();
 });
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 100000,
+})
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err.message));
 
