dfb2e40442de375ee671562583cc069edc472814
piyushagarwal-55 <piyushaga2005@gmail.com>
2025-02-03 23:51:28 +0530

    vercel push4

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const serverless = require('serverless-http');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+// const PORT = process.env.PORT || 8000;
 
 app.use(morgan('dev'));
 app.set('view engine' , 'ejs');
